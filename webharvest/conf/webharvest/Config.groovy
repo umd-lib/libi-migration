e402b4488cb2427f2e0849bba6e14050e4abce62
Ben Wallberg <wallberg@umd.edu>
2009-06-03 18:46:48 +0000

    recognize another sidebar pattern
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/libi-migration@803 983cd692-402e-0410-a370-846aaee464cf

--- webharvest/conf/webharvest/Config.groovy
+++ webharvest/conf/webharvest/Config.groovy
@@ -241,7 +241,7 @@ class Config {
     }
 
     // Is there a sidebar we can tack onto the end?
-    l = doc.selectNodes('//table//table/tbody/tr/td[h4]')
+    l = doc.selectNodes("//table//table/tbody/tr/td[h4]|//table//table/tbody/tr/td[span[@class='leftcol_heading']]")
     if (l.size() > 0) {
       div = l[0].clone()
       div.name = 'div'
