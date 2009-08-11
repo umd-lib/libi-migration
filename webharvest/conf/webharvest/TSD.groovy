f7be208f6820373d3cf87ff089e8242854352703
Ben Wallberg <wallberg@umd.edu>
2009-08-11 18:43:58 +0000

    handle LAN files for TSD
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/libi-migration@891 983cd692-402e-0410-a370-846aaee464cf

--- webharvest/conf/webharvest/TSD.groovy
+++ webharvest/conf/webharvest/TSD.groovy
@@ -3,11 +3,14 @@ package webharvest
 import org.apache.log4j.Logger;
 
 import org.dom4j.Node
+import org.dom4j.DocumentHelper
 
 
 class TSD extends Config {
 
   def private static log = Logger.getInstance(TSD.getName());
+
+  def tsd = new File('/r/department/Technical Services/TSDWWW/TSDPOLPRO')
 
 
   /**********************************************************************/
@@ -18,12 +21,100 @@ class TSD extends Config {
   public TSD() {
     super()
 
+    if (! tsd.exists()) {
+      throw new Exception("TSD WWW is not mounted")
+    }
+
     baseUrl = new URL('http://www.lib.umd.edu/TSD/tsd_policies2.html')
 
     followable = [
-      ['^http://www.lib.umd.edu/TSD/.*'             // incl
-      ]
+      '^http://www.lib.umd.edu/TSD.*',             // include
     ]
   }
+
+  /**********************************************************************/
+  /**
+   * Build a new url. Perform normalization.
+   */
+
+  public URL buildUrl(URL baseUrl, String rel) {
+    def m = rel =~ /http:\/\/libdoc.*\/tsdwww\/tsdpolpro\/(.*)/
+    if (m) {
+      // LAN file
+      def f = new File(tsd, m[0][1])
+      if (! f.exists()) {
+        throw new Exception("${f} does not exist")
+      }
+
+      return f.toURI().toURL()
+    }
+
+    return super.buildUrl(baseUrl, rel)
+  }
+
+
+  /**********************************************************************/
+  /**
+   * Get the content-type of one url.
+   */
+
+  public String getContentType(URL url) {
+
+    if (url.protocol == 'file') {
+      // LAN file
+      return 'application/lanfile'
+    }
+
+    return super.getContentType(url)
+  }
+
+
+  /**********************************************************************/
+  /**
+   * Handle a file for download.
+   */
+
+  public void handleFile(Page page) {
+
+    if (page.url.protocol == 'file') {
+      // LAN file
+
+      page.download = getDownload(page)
+
+      log.debug("Download file name: ${page.download}")
+
+      // copy LAN file to work file
+      page.download << page.url.openStream()
+
+      // create dummy content
+      def doc = DocumentHelper.createDocument()
+      doc.addElement('html').addElement('body')
+
+      def body = DocumentHelper.createDocument()
+      body.addElement('div')
+    
+      savePage(page, doc, body)
+      
+      return
+    }
+
+    super.handleFile(page)
+  }
+
+
+  /**********************************************************************/
+  /**
+   * Determine if a url should be followed
+   */
+
+  public boolean isFollowable(Page page) {
+    if (page.url.protocol == 'file') {
+      // LAN file
+      return true;
+    }
+
+    return super.isFollowable(page)
+  }
+
 
 }
