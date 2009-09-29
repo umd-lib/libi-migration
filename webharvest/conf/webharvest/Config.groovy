54f83ef08cc9b7d13af0e8392a0945adb0188a48
Ben Wallberg <wallberg@umd.edu>
2009-09-29 12:37:02 +0000

    handle more old TSD bodies
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/libi-migration@957 983cd692-402e-0410-a370-846aaee464cf

--- webharvest/conf/webharvest/Config.groovy
+++ webharvest/conf/webharvest/Config.groovy
@@ -373,7 +373,7 @@ class Config {
     def div
 
     // first, try to recognize some patterns
-    l = doc.selectNodes("//h1|//h2|//span[@class='breadcrumbs']")
+    l = doc.selectNodes("/html/body/center[//h1]|//h1|//h2|//span[@class='breadcrumbs']")
     if (l.size() > 0) {
       def top = l[0].parent
 
