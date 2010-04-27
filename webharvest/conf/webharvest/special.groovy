c0759b14fccc0ad307d21aefdea5785d59fd6ca8
Ben Wallberg <wallberg@umd.edu>
2010-04-27 14:11:32 +0000

    add special Minutes Archive
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/libi-migration@1377 983cd692-402e-0410-a370-846aaee464cf

--- webharvest/conf/webharvest/special.groovy
+++ webharvest/conf/webharvest/special.groovy
@@ -31,4 +31,35 @@ class special extends Config {
       '^http://www.lib.umd.edu/CLMD/SpecColl/collaborative/reports.*',
     ]
   }
+
+  /**********************************************************************/
+  /**
+   * Build the tree of nodes.
+   */
+
+  public List buildTree(List done) {
+
+    done.each { page ->
+      if (page.surl == 'http://www.lib.umd.edu/CLMD/SpecColl/collaborative/minutes.html') {
+
+        // Make the minutes page look like the parent folder of all minutes
+
+        def hbpage = hb.get(Page, page.surl)
+
+        page.url = new URL('http://www.lib.umd.edu/CLMD/SpecColl/collaborative/minutes/')
+        page.title = 'Minutes Archive'
+
+        def hbpageNew = hbpage.clone()
+        hbpageNew.body = hbpage.body 
+
+        hbpageNew.url = page.url
+        hbpageNew.title = page.title
+
+        hb.save(hbpageNew)
+      }
+    }
+
+    return super.buildTree(done)
+  }
+
 }
