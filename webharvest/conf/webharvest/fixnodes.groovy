c9bc93d919f5fc7856898c447ced9291d8f34b6e
Ben Wallberg <wallberg@umd.edu>
2010-06-11 11:40:14 +0000

    use command line processing in fixnodes
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/libi-migration@1488 983cd692-402e-0410-a370-846aaee464cf

--- webharvest/conf/webharvest/fixnodes.groovy
+++ webharvest/conf/webharvest/fixnodes.groovy
@@ -8,11 +8,23 @@ import org.htmlcleaner.DomSerializer
 import org.htmlcleaner.HtmlCleaner
 import org.htmlcleaner.PrettyXmlSerializer
 
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.PosixParser
+
+
+infile = null
+outfile = null
+parent = false
 
 // Fixup nodes
 //   1. make body valid xml
  
 def main() {
+  parseCommandLine()
+
   cleaner = new HtmlCleaner()     // html cleanup
   props = cleaner.getProperties()
   ds = new DomSerializer(props)
@@ -20,11 +32,8 @@ def main() {
   df = new DocumentFactory()      // dom4j utilities
   dr = new DOMReader(df)
 
-  infile = new File(args[0])
-
   saxreader = new SAXReader()
   doc = saxreader.read(infile)
-
 
   doc.selectNodes('//node/data/body').each { n ->
     // Cleanup
@@ -39,8 +48,65 @@ def main() {
     n.text = body.getRootElement().asXML()
   }
 
-  println doc.asXML().replace('&amp;','&')
+  outfile << doc.asXML().replace('&amp;','&')
 }
+
+/**********************************************************************/
+/*
+ * Parse the command line.
+ */
+
+def parseCommandLine() {
+  // Setup the options
+  options = new Options()
+  
+  option = new Option("i", "infile", true, "input file")
+  option.setRequired(true)
+  option.setType(File)
+  options.addOption(option)
+
+  option = new Option("o", "outfile", true, "output file")
+  option.setRequired(true)
+  option.setType(File)
+  options.addOption(option)
+
+  option = new Option("p", "parent", false, "create parent node with links to all others")
+  option.setRequired(false)
+  options.addOption(option)
+
+  // Parse the command line
+  parser = new PosixParser()
+  try {
+    cmd = parser.parse(options, args)
+  }
+  catch (Exception e) {
+    printUsage(options, e.getMessage())
+  }
+
+  // Validate results
+  infile = cmd.getOptionObject('i')
+  outfile = cmd.getOptionObject('o')
+  parent = cmd.hasOption('p')
+
+}
+
+
+/**********************************************************************/
+/*
+ * Print program usage.
+ */
+
+def printUsage(Options options, Object[] args) {
+  // print messages
+  args.each {println it}
+  if (args.size() != 0) { println '' }
+
+  formatter = new HelpFormatter()
+  formatter.printHelp("fixnodes -i <infile> -o <outfile> [-p] \n", options)
+
+  System.exit(1)
+}
+
 
 main()
 
